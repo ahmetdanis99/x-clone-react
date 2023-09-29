cd29475c416034271cbcc26915d031a4e6d35e81
mumbarsurat <77283856+mumbarsurat@users.noreply.github.com>
2023-09-29 21:54:28 +0300

    v 0.1
    SideBar ve Menu neredeyse bitti

---
+++ src/routes/index.jsx
@@ -0,0 +1,58 @@
+import { createBrowserRouter } from "react-router-dom";
+import MainLayout from "~/layouts/main";
+import Bookmark from "~/pages/bookmark";
+import Community from "~/pages/community";
+import Explore from "~/pages/explore";
+import Home from "~/pages/home";
+import List from "~/pages/list";
+import Message from "~/pages/message";
+import NotFound from "~/pages/not-found";
+import Notifications from "~/pages/notifications";
+import Profile from "~/pages/profile";
+
+const routes = createBrowserRouter([
+  {
+    path: "/",
+    element: <MainLayout />,
+    children: [
+      {
+        index: true,
+        element: <Home />,
+      },
+      {
+        path: "/explore",
+        element: <Explore />,
+      },
+      {
+        path: "/notifications",
+        element: <Notifications />,
+      },
+      {
+        path: "/messages",
+        element: <Message />,
+      },
+      {
+        path: "/lists",
+        element: <List />,
+      },
+      {
+        path: "/bookmarks",
+        element: <Bookmark />,
+      },
+      {
+        path: "/communities",
+        element: <Community />,
+      },
+      {
+        path: "/profile",
+        element: <Profile />,
+      },
+    ],
+  },
+  {
+    path: "*",
+    element: <NotFound />,
+  },
+]);
+
+export default routes;
